1a98c8abe53aa9d4f6da606fddfa564e0fbe6ab0
jen6 <fjen6b@gmail.com>
2017-05-19 00:43:06 +0900

    split main function to another file

---
+++ main.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "perceptron.cc"
+int main()
+{
+   Perceptron<3> or_percep;
+   //answer x0 x1
+   std::vector<std::bitset<3>> and_train = {
+       std::bitset<3>("000"),
+       std::bitset<3>("010"),
+       std::bitset<3>("001"),
+       std::bitset<3>("111")
+   };
+   bool result = or_percep.DoTrain(and_train);
+   if(result)
+       std::cout << "traind" << std::endl;
+   else
+       std::cout << "not trained" <<std::endl;
+}
--- perceptron.cc
+++ perceptron.cc
@@ -100,19 +100,3 @@ public:
     }
 };
 
-int main()
-{
-   Perceptron<3> or_percep;
-   //answer x0 x1
-   std::vector<std::bitset<3>> and_train = {
-       std::bitset<3>("000"),
-       std::bitset<3>("010"),
-       std::bitset<3>("001"),
-       std::bitset<3>("111")
-   };
-   bool result = or_percep.DoTrain(and_train);
-   if(result)
-       std::cout << "traind" << std::endl;
-   else
-       std::cout << "not trained" <<std::endl;
-}
